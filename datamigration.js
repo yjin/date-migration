ce8b1e43034742c30f85360197f22aa9b2a1f5e6
yanjinww <yan.jin@weightwatchers.com>
2018-08-08 16:56:15 +0800

    rename files, add dashboard page

--- datamigration.js
+++ datamigration.js
@@ -1,3 +1,4 @@
+
 var JobItemComponent = {
 	props: {
 		title: String,
@@ -8,32 +9,53 @@ var JobItemComponent = {
 	template: '<li><a :href="url">{{ title }}</a> - {{ status }} - {{ create }}</li>'
 }
 
-var data = {
+const data = {
 		jobList: [
 			{
 				id: 0,
 				title: 'Job-01',
-				status: 'Started',
-				create: Date(Date.now()),
+				status: 'Complete',
+				create: 'Tue Aug 07 2018 14:37:53 GMT+0800 (China Standard Time)',
 				url: './detail.html?id=0'
 			},
 			{
 				id: 1,
 				title: 'Job-02',
-				status: 'Started',
-				create: Date(Date.now()),
+				status: 'Complete',
+				create: 'Tue Aug 07 2018 14:37:53 GMT+0800 (China Standard Time)',
 				url: './detail.html?id=1'
+			},
+			{
+				id: 2,
+				title: 'Job-03',
+				status: 'Running',
+				create: 'Tue Aug 07 2018 14:37:53 GMT+0800 (China Standard Time)',
+				url: './detail.html?id=2'
+			},
+			{
+				id: 3,
+				title: 'Job-04',
+				status: 'Started',
+				create: 'Tue Aug 07 2018 14:37:53 GMT+0800 (China Standard Time)',
+				url: './detail.html?id=3'
 			}
 		],
-		id: 2
+		id: 0
 }
 
 var app = new Vue({
+
 	el: '#data-migration',
+
 	components: {
 		'job-item' : JobItemComponent
 	},
+
 	data: data,
+	created: function () {
+		this.id = this.jobList.length;
+	},
+
 	computed: {
 		urlLink: function () {
 			// var job = this.jobList.filter(function(item) {
@@ -57,8 +79,27 @@ var app = new Vue({
 			var id = this.urlParamsValueForKey('id');
 			var job = this.jobList[id];
 			return job.create;
+		},
+		numberOfCompleteJobs: function () {
+			let jobs = this.jobList;
+			return jobs.filter( (job) => {
+				return job.status === 'Complete';
+			}).length;
+		},
+		numberOfRunningJobs: function () {
+			let jobs = this.jobList;
+			return jobs.filter( (job) => {
+				return job.status === 'Running';
+			}).length;
+		},
+		numberOfNewJobs: function () {
+			let jobs = this.jobList;
+			return jobs.filter( (job) => {
+				return job.status === 'Started';
+			}).length;
 		}
 	},
+
 	methods: {
 		urlParamsValueForKey: function (key) {
 			return new URLSearchParams(window.location.search).get(key);
